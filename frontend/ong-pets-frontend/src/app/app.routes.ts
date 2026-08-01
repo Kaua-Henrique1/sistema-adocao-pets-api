81804c8ef3f4b9af52ee0e73054e696a999c1c57
Kauã Henrique <kaua2006henrique023@gmail.com>
2026-08-01 11:59:33 -0300

    feat(login): adiciona configuração para verificar se está logado

--- frontend/ong-pets-frontend/src/app/app.routes.ts
+++ frontend/ong-pets-frontend/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { Login } from './components/login/login';
 import { Home } from './components/home/home';
 import { Pets } from './components/pets/pets';
 import { Pessoas } from './components/pessoas/pessoas';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'Login', pathMatch: 'full' },
   { path: 'Login', component: Login },
-  { path: 'home', component: Home },
-  { path: 'pets', component: Pets },
-  { path: 'pessoas', component: Pessoas },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+  { path: 'pets', component: Pets, canActivate: [authGuard] },
+  { path: 'pessoas', component: Pessoas, canActivate: [authGuard] },
   { path: '**', redirectTo: 'Login' } 
 ];

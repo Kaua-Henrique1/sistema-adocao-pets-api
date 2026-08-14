3fb0db0939e417585942ad64fe8271e78c335c42
Kauã Henrique <kaua2006henrique023@gmail.com>
2026-08-14 10:56:30 -0300

    build(deps): adiciona dependencia do banco de dados h2

--- backend/src/test/java/devKaua/projeto/SistemaAdocaoPetsApiApplicationTests.java
+++ backend/src/test/java/devKaua/projeto/SistemaAdocaoPetsApiApplicationTests.java
@@ -2,8 +2,10 @@ package devKaua.projeto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SistemaAdocaoPetsApiApplicationTests {
 
     @Test

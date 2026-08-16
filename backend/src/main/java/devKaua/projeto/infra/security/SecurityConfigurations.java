870e870822ef6e3fc2d22979a5184383dea781e1
Kauã Henrique <kaua2006henrique023@gmail.com>
2026-08-16 09:52:10 -0300

    fix(security): adiciona origin do container nginx ao CORS

--- backend/src/main/java/devKaua/projeto/infra/security/SecurityConfigurations.java
+++ backend/src/main/java/devKaua/projeto/infra/security/SecurityConfigurations.java
@@ -70,7 +70,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080", "http://localhost"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

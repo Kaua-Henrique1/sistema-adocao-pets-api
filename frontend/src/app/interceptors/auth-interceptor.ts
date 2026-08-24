b68f1def788c8283df11989e9bef0aee212dbdb5
Kauã Henrique <kaua2006henrique023@gmail.com>
2026-08-24 15:46:49 -0300

    feat(auth): adiciona tratamento para quando expirar token

--- frontend/src/app/interceptors/auth-interceptor.ts
+++ frontend/src/app/interceptors/auth-interceptor.ts
@@ -1,16 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
-
+  let request = req;
   if (token) {
-    const clonedReq = req.clone({
+    request = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
-    return next(clonedReq);
   }
 
-  return next(req);
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem('token');
+        if (authService.logout) {
+          authService.logout();
+        }
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    }),
+  );
 };
